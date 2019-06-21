57ed740c106c50ac0cac41107998018068f0dc90
Brandon Harris <BrandonMGH@Yahoo.com>
2019-06-20 23:06:47 -0700

    updated styling on title component and adjusted width length on all components to fit body better

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main/Main';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+      families: ['Permanent Marker']
+    }
+  });
 
 
 ReactDOM.render(<Main />, document.getElementById('root'));
