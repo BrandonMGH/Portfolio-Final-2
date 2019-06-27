f463d68330aec97fdb7524f97897413e37746d51
Brandon Harris <BrandonMGH@Yahoo.com>
2019-06-27 15:14:35 -0700

    changed font style to 'Permanent Marker' for all fonts on all components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import WebFont from 'webfontloader';
 
 WebFont.load({
     google: {
-      families: ['Permanent Marker']
+      families: ['Permanent Marker','Pathway Gothic One','Rubik Mono One']
     }
   });
 
