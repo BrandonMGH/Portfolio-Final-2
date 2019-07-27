f049226c8c099ee0c60bd385d67aca3932e9eebf
Brandon Harris <BrandonMGH@Yahoo.com>
2019-07-27 13:04:18 -0700

    updated media query styling on PortfolioProjects.js and PortfolioProjects.css

--- src/Components/Portfolio/PortfolioProjects/PortfolioProjects.js
+++ src/Components/Portfolio/PortfolioProjects/PortfolioProjects.js
@@ -44,43 +44,41 @@ class PortfolioProjects extends Component {
         <Row>
           <Col>
             <h1 className="portfolioProjectTitle" align="middle">-PORTFOLIO-</h1>
-            <Row >
-              <div className="test">
-              <Col>
+            <Row> 
+              <Col className="mediaQ">
                 <div onClick={this.handleButtonClickOne} className="box">
                   <img className="CarouselImg" src={got2} align="middle"></img>
                   <div className="text">
                     <h2>JAVASCRIPT PROJECTS</h2>
 
                   </div>
-                </div>
-              </Col>
-              </div>
-              <Col>
+                  </div>
+                </Col> 
+                <Col className="mediaQ">
                 <div onClick={this.handleButtonClickTwo} className="box">
                   <img className="CarouselImg" src={chatbox} align="middle"></img>
                   <div className="text">
                     <h2>NODE PROJECTS</h2>
                   </div>
                 </div>
-              </Col>
-              <Col>
+                </Col> 
+                <Col className="mediaQ">
                 <div onClick={this.handleButtonClickThree} className="box">
                   <img className="CarouselImg" src={cameragirl} align="middle"></img>
                   <div className="text">
                     <h2>REACT PROJECTS</h2>
                   </div>
                 </div>
-              </Col>
-              <Col>
+                </Col>
+                <Col className="mediaQ">
                 <div onClick={this.handleButtonClickFour} className="box">
                   <img className="CarouselImg" src={codeall} align="middle"></img>
                   <div className="text">
                     <h2>FAVORITE PROJECTS</h2>
                   </div>
                 </div>
-              </Col>
-            </Row>
+                </Col>
+                </Row>
           </Col>
           <div className="White">{this.state.shownProject}</div>
         </Row>
